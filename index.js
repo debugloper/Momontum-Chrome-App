e320a66fe71a821f697170523209b181c380305d
Ju Young Kim <juyoung.kim517@gmail.com>
2021-02-03 00:46:21 +0200

    test

--- index.js
+++ index.js
@@ -4,3 +4,9 @@ a = 4; // you update the variable a so don't use 'let' to initialise
 
 const var1 = 221; // you can't update const variable
 console.log(b);
+
+const testObject = {
+    name: "test"
+}
+
+console.log(testObject.name)
