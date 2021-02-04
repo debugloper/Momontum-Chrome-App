db0a55e4c86fc045ceca89c8ee3b83a842097612
Ju Young Kim <juyoung.kim517@gmail.com>
2021-02-05 01:07:31 +0200

    toggle function

--- index.js
+++ index.js
@@ -1,10 +1,25 @@
 // define function inside an object
-const title = document.getElementById("title");
+// const title = document.getElementById("title");
 
 // DOM document object model
 // Everything you select from the page using JS will be called into an object.
-title.innerHTML = "This is a test again"
-title.style.color = "black"
+// title.innerHTML = "This is a test again"
+// title.style.color = "black"
 
 // Change HTML using JavaScript.
-document.title = "Momontum"
+// document.title = "Momontum"
+
+const title = document.querySelector("#title");
+
+const CLICKED_CLASS = "clicked";
+
+function handleClick() {
+    // if CLICKED_CLASS nto in className, add if exists, remove using toggle.
+    title.classList.toggle(CLICKED_CLASS);
+}
+
+function init() {
+    title.addEventListener("click", handleClick);
+}
+
+init();
