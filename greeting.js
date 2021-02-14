ee93cf781a3a0cd0738cfc0898d0809a093e06a4
Ju Young Kim <juyoung.kim517@gmail.com>
2021-02-14 23:07:57 +0200

    modify to save the input data

--- greeting.js
+++ greeting.js
@@ -4,8 +4,21 @@ const greeting = document.querySelector(".js-greetings");
 const USER_LS = "currentUser";
 const SHOWING_CN = "showing";
 
+function saveName(text) {
+    localStorage.setItem(USER_LS, text);
+}
+
+function handleSubmit(event) {
+    // prevent default behaviour of event
+    event.preventDefault();
+    const currentValue = input.value;
+    paintGreeting(currentValue);
+    saveName(currentValue);
+}
+
 function askForName() {
     form.classList.add(SHOWING_CN);
+    form.addEventListener("submit", handleSubmit)
 }
 
 function paintGreeting(text) {
